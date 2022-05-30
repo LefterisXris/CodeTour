f2b0cd3059074982dd5a84555144824bb05b8381
Eleftherios Chrysochoidis <lefterisxris@gmail.com>
2022-05-30 19:48:01 +0300

    fix: Fixed backwards compatibility with IntelliJ 213.* due to MultiLineDialog absence
    Signed-off-by: Eleftherios Chrysochoidis <lefterisxris@gmail.com>

--- src/main/java/org/uom/lefterisxris/codetour/tours/ui/StepDescriptionEditor.java
+++ src/main/java/org/uom/lefterisxris/codetour/tours/ui/StepDescriptionEditor.java
@@ -3,12 +3,14 @@ package org.uom.lefterisxris.codetour.tours.ui;
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapperPeer;
-import com.intellij.openapi.ui.MessageMultilineInputDialog;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.TextRange;
+import com.intellij.ui.components.JBScrollPane;
 import org.uom.lefterisxris.codetour.tours.domain.Step;
 
+import javax.swing.*;
 import javax.swing.text.JTextComponent;
+import java.awt.*;
 
 /**
  * Editor (as dialog) for Step's description value
@@ -16,7 +18,7 @@ import javax.swing.text.JTextComponent;
  * @author Eleftherios Chrysochoidis
  * Date: 29/5/2022
  */
-public class StepDescriptionEditor extends MessageMultilineInputDialog {
+public class StepDescriptionEditor extends Messages.InputDialog {
 
    private StepDescriptionEditor(Project project, Step step) {
       super(project, String.format("Edit Step's '%s' description", step.getTitle()),
@@ -31,6 +33,34 @@ public class StepDescriptionEditor extends MessageMultilineInputDialog {
 
       dialog.show();
       return dialog.getInputString();
+   }
+
+   @Override
+   protected JTextComponent createTextFieldComponent() {
+      return new JTextArea(7, 50);
+   }
+
+   @Override
+   protected JComponent createScrollableTextComponent() {
+      return new JBScrollPane(myField);
+   }
+
+   @Override
+   protected JComponent createNorthPanel() {
+      return null;
+   }
+
+   @Override
+   protected JComponent createCenterPanel() {
+      JPanel messagePanel = new JPanel(new BorderLayout());
+      if (myMessage != null) {
+         JComponent textComponent = createTextComponent();
+         messagePanel.add(textComponent, BorderLayout.NORTH);
+      }
+
+      myField = createTextFieldComponent();
+      messagePanel.add(createScrollableTextComponent(), BorderLayout.CENTER);
+      return messagePanel;
    }
 
    /**
